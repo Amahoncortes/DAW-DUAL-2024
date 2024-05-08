cc0a89ba7792885938f48416c5d91c0ede6e2a3d
Abel <Amahoncortes@gmail.com>
2024-05-08 07:52:41 +0200

    feat: Esqueleto de base de datos relacional - tema 12 POO

---
+++ Programación/Tema 12 - Persistencia en bases de datos relacionales/BibliotecaAbel/src/main/java/edu/gorillas/Metodos/Crear.java
@@ -0,0 +1,40 @@
+package edu.gorillas.Metodos;
+
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Crear {
+
+    public static void crearBase(Statement sentencia) {
+        try {
+            //Nombre de la base de datos
+            String bd = "Biblioteca";
+
+            //Creacion de la base de datos
+            sentencia.execute("CREATE DATABASE IF NOT EXISTS " + bd + ";");
+            sentencia.execute("USE "+ bd +";");
+            //Creacion de la tabla Autores
+            sentencia.execute("CREATE TABLE IF NOT EXISTS Autores ("
+                    + "dni char(9) primary key not null,"
+                    + "Nombre varchar(20) not null,"
+                    + "Nacionalidad varchar(20) not null)");
+
+            //Creacion de la tabla Libros
+            sentencia.execute("CREATE TABLE IF NOT EXISTS Libros ("
+                    + "IdLibro int(5) primary key auto_increment not null,"
+                    + "Titulo varchar(20) not null,"
+                    + "Precio float not null,"
+                    + "Autor varchar(9) not null,"
+                    + "FOREIGN KEY(Autor) REFERENCES Autores(dni)"
+                    + " ON DELETE CASCADE "
+                    + " ON UPDATE CASCADE)");
+
+
+            //Creacion de la tabla Prestamos
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
---
+++ Programación/Tema 12 - Persistencia en bases de datos relacionales/BibliotecaNuria/src/main/java/edu/gorillas/Metodos/Insertar.java
@@ -0,0 +1,75 @@
+package edu.gorillas.Metodos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Insertar {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void mainInsertar(Statement sentencia, Scanner sc) {
+        int opcion = 0;
+
+        do {
+            opcion = Menu.menuInsertar(sc);
+            switch (opcion) {
+                case 1:
+                    Insertar.nuevoAutor(sentencia);
+                    break;
+                case 2:
+                    Insertar.nuevoLibro(sentencia);
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        } while (opcion != 3);
+    }
+
+    private static void nuevoAutor(Statement sentencia) {
+        String dni, nacionalidad, nombre;
+        try {
+            System.out.println("Dame el dni del autor");
+            dni = sc.nextLine();
+            System.out.println("Dame el nombre del autor");
+            nombre = sc.nextLine();
+            System.out.println("Dame la nacionalidad del autor");
+            nacionalidad = sc.nextLine();
+
+            sentencia.executeUpdate("INSERT INTO autor VALUES ('" + dni + "', '" + nombre + "', '" + nacionalidad + "')");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void nuevoLibro(Statement sentencia) {
+        System.out.println("Dame el titulo del libro");
+        String titulo = sc.nextLine();
+        System.out.println("Dame el precio del libro");
+        double precio = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("Dame el nombre del autor");
+        String autorNombre = sc.nextLine();
+
+        try {
+            ResultSet rs = sentencia.executeQuery("SELECT dni FROM autor WHERE nombre = '" + autorNombre + "'");
+
+            if (rs.next()) {
+                String dni = rs.getString("dni");
+                sentencia.executeUpdate("INSERT INTO libro VALUES (Titulo, Precio, Autor) VALUES ('" + titulo + "', '" + precio + "', '" + autorNombre + "')");
+                System.out.println("Libro insertado en base de datos.");
+            } else {
+                System.out.println("Autor no encontrado");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
+
